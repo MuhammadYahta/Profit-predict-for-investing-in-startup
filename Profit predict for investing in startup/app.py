3618aa440064bbc54c2cbd81796a6f04a3a7c576
Muhammad Yahya <34463952+MuhammadYahta@users.noreply.github.com>
2019-03-31 01:55:06 +0800

    Add files via upload

---
+++ Profit predict for investing in startup/app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request
+from sklearn.externals import joblib
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+#@app.route('/test')
+#
+#def test():
+#    return "Flask is being used for Development"
+
+
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/predict", methods = ['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        print(request.form.get('NewYork'))
+        try:
+            NewYork = float(request.form['NewYork'])
+            
+            California = float(request.form['California'])
+            
+            Florida = float(request.form['Florida'])
+          
+            RnD_Spend = float(request.form['RnD_Spend'])
+           
+            Admin_Spend = float(request.form['Admin_Spend'])
+            
+            Market_Spend = float(request.form['Market_Spend'])
+            
+            pred_args = [NewYork, California, Florida, RnD_Spend, Admin_Spend, Market_Spend]
+           
+            pred_args_arr = np.array(pred_args)
+            
+            pred_args_arr = pred_args_arr.reshape(1,-1)
+            
+            mul_reg = open("multiple.linear.pkl", "rb")
+            
+            
+            ml_model = joblib.load(mul_reg)
+            
+            model_prediction = ml_model.predict(pred_args_arr)
+            model_prediction = round(float(model_prediction),2) 
+        except valueError:
+            return "Please check if the values are entered correctly"
+
+        return render_template('predict.html', prediction = model_prediction)
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
